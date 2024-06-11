7543e2f1650dd665a08de57c74e69f959d722567
SadeghHayeri <hayerisadegh@gmail.com>
2024-06-11 18:29:21 +0330

    Add gson

--- telegram/src/main/kotlin/com/github/kotlintelegrambot/entities/LinkPreviewOptions.kt
+++ telegram/src/main/kotlin/com/github/kotlintelegrambot/entities/LinkPreviewOptions.kt
@@ -1,5 +1,6 @@
 package com.github.kotlintelegrambot.entities
 
+import com.google.gson.Gson
 import com.google.gson.annotations.SerializedName
 
 data class LinkPreviewOptions(
@@ -8,4 +9,10 @@ data class LinkPreviewOptions(
     @SerializedName("prefer_small_media") val preferSmallMedia: Boolean? = null,
     @SerializedName("prefer_large_media") val preferLargeMedia: Boolean? = null,
     @SerializedName("show_above_text") val showAboveText: Boolean? = null,
-)
+) {
+    companion object {
+        val GSON = Gson()
+    }
+
+    override fun toString(): String = GSON.toJson(this)
+}
